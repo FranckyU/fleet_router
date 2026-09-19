ce336400cc7b86134908f93f61e625e0131fd240
Andy Kifer <kifer.mada@gmail.com>
2026-09-19 12:56:07 +0300

    Add the models into admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+from api.models import CachedRoute, FuelStation
+
+
+@admin.register(FuelStation)
+class FuelStationAdmin(admin.ModelAdmin):
+    list_display  = ("name", "city", "state", "price", "is_geocoded")
+    list_filter   = ("state",)
+    search_fields = ("name", "city", "opis_id")
+
+
+@admin.register(CachedRoute)
+class CachedRouteAdmin(admin.ModelAdmin):
+    list_display  = ("start_query", "finish_query",
+                     "total_km", "total_miles", "total_cost", "created_at")
+    search_fields = ("start_query", "finish_query")
+
